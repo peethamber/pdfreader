83afd0b4d594abf198a043c18eed6afec1246f32
peethamber <40764612+peethamber@users.noreply.github.com>
2025-04-12 20:52:53 +0530

    Update pdfreader.py

--- pdfreader.py
+++ pdfreader.py
@@ -21,7 +21,7 @@ selected_file_path = file_selector()
 
 if selected_file_path and selected_file_path.lower().endswith('.pdf'):
     with st.status("Loading File.."):
-        pdf_viewer(selected_file_path,width="90%")
+        pdf_viewer(selected_file_path,width="130%")
     st.write("File Loaded. Use pull down to view")   
 else:
     st.write(" Choose a PDF File to View")
