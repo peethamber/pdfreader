897612774438434d00eee133e7db4621c2c73b27
peethamber <40764612+peethamber@users.noreply.github.com>
2025-04-12 17:38:04 +0530

    Update pdfreader.py

--- pdfreader.py
+++ pdfreader.py
@@ -7,6 +7,7 @@ def file_selector(folder_path='.'):
     selected_filename = st.selectbox('Select a file from list below', filenames)
     return os.path.join(folder_path, selected_filename)
 
+st.title("View Lesson Files")
 selected_file_path = file_selector()
 if selected_file_path and selected_file_path.endswith('.Pdf'):
     pdf_viewer(selected_file_path)
