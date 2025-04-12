2c05378a70263f4b87d22931402bb838752a5f7b
peethamber <40764612+peethamber@users.noreply.github.com>
2025-04-12 17:33:19 +0530

    Update pdfreader.py

--- pdfreader.py
+++ pdfreader.py
@@ -1,4 +1,11 @@
 import streamlit as st
 from streamlit_pdf_viewer import pdf_viewer
 
-pdf_viewer("Testdoc.Pdf")
+def file_selector(folder_path='.'):
+    filenames = os.listdir(folder_path)
+    selected_filename = st.selectbox('Select a file from list below', filenames)
+    return os.path.join(folder_path, selected_filename)
+
+elected_file_path = file_selector()
+if selected_file_path and selected_file_path.endswith('.Pdf'):
+    pdf_viewer(selected_file_path)
